c3c8789f89589c147041984efa1ab725c74bf661
anna_bezuglenko <a.bezuglenko@ivideon.com>
2021-12-01 10:36:51 +0300

    task 4 finished

--- task_4/solution.js
+++ task_4/solution.js
@@ -14,6 +14,7 @@ function renderProductsCards(json) {// Напишите функцию renderPro
 clearProducts ();// Выполните функцию clearProducts - она уже написана в коде, просто вызовите ее, аргументы не требуются
 let products = parseProducts(json);// Выполните функцию parseProducts, передав в неё json - аргумент функции и запишите результат в переменную products
 length = products.length;// Запишите в переменную length значение свойства products.length
-for (let i = 0; i < length; i++ ) {// Напишите цикл, в котором перебираете все products от 0 до (length - 1)
-    addProduct(products[i]); } // и выполняете addProduct с каждым элементом массива products (передавая элемент массива как аргумент)
+for (let i = 0; i < length; i+=1 ) {// Напишите цикл, в котором перебираете все products от 0 до (length - 1)
+    addProduct(products[i]); 
+} // и выполняете addProduct с каждым элементом массива products (передавая элемент массива как аргумент)
 }
